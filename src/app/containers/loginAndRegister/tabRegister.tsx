214d44d50f0fd00c4b0bf5f1b68964751084b088
ngoChungAAu <audavn@gmail.com>
2021-10-12 18:01:57 +0700

    au fix conflict

--- src/app/containers/loginAndRegister/tabRegister.tsx
+++ src/app/containers/loginAndRegister/tabRegister.tsx
@@ -19,7 +19,6 @@ interface IFormInput {
 }
 
 const AU_SITE_KEY: string = '6LcSG9EaAAAAABvbpHkdugGmjEWeYPp6NoPPDEvt';
-const AU_SECRET_KEY: string = '6Lc8HrgcAAAAAEHCvI0OVfuCY3hWCGyQE3DvgdPV';
 
 const TabPaneLogin = styled(TabPane)`
   padding-top: 3rem;
